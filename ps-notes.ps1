a684b1a5ac18e9e807ebf18d164fe7b3a460c27e
Ledbetter <Matthew.Ledbetter@sbdinc.com>
2017-03-15 14:38:08 -0400

    xml document

---
+++ ps-notes.ps1
@@ -0,0 +1,34 @@
+﻿# list folders in directory
+# Get-ChildItem -Path ./ -Directory
+
+# search for commandlets that contain the word service.
+# Get-Help *service*
+
+# testing looping with foreach statement.
+# $list = Get-Service
+# foreach ($item in $list) {
+#     $sn = $item.Name + ", " + $item.Status
+#     # learning powershell condition statements.
+#     if ($item.Status -eq "Running") {
+#         Write-Output $sn
+#     }
+# }
+
+# Get-Help Get-Service -ShowWindow
+
+# Clear-Host
+
+# Get-Service -Name BFE, BITS
+
+# XmlNodeAdaptor Working with XML files
+$path = "$(Get-Location)\docs\plant_catalog.xml"
+$xmlDoc = New-Object Xml.XmlDocument
+$xmlDoc.Load($path)
+#$xmlDoc | Get-Member
+#$plants = $xmlDoc.GetElementsByTagName("PLANT")
+#$plants[0]
+$plants[0].COMMON
+$plants[0].BOTANICAL
+$plants[0].ZONE
+$plants[0].PRICE
+
