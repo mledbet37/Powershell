16ac47aecc8e81ec17fd9dd8cde1a20d218d96c5
Ledbetter <Matthew.Ledbetter@sbdinc.com>
2017-03-15 09:25:39 -0400

    initial commit

---
+++ examples.ps1
@@ -0,0 +1,23 @@
+﻿# list folders in directory
+Get-ChildItem -Path ./ -Directory
+
+# search for commandlets that contain the word service.
+Get-Help *service*
+
+# testing looping with foreach statement.
+$list = Get-Service
+foreach ($item in $list) {
+    $sn = $item.Name + ", " + $item.Status
+    # learning powershell condition statements.
+    if ($item.Status -eq "Running") {
+        echo $sn
+    }
+}
+
+
+Get-Help Get-Service -ShowWindow
+
+cls
+
+
+Get-Service -Name BFE, BITS
